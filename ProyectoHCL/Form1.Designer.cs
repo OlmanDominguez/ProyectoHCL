1fedbc217d509ad9eecd335478a74a3284ef175a
OlmanDomínguez <gersondominguez.21@gmail.com>
2023-06-04 00:55:36 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoHCL/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace ProyectoHCL
+{
+    partial class FORMULARIO
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FORMULARIO));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UsuarioBox1 = new System.Windows.Forms.TextBox();
+            this.ContraseñaBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(407, 256);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresa tu nombre de Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(407, 343);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingresa tu contraseña";
+            // 
+            // UsuarioBox1
+            // 
+            this.UsuarioBox1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.UsuarioBox1.Location = new System.Drawing.Point(407, 298);
+            this.UsuarioBox1.Name = "UsuarioBox1";
+            this.UsuarioBox1.PasswordChar = '*';
+            this.UsuarioBox1.Size = new System.Drawing.Size(319, 31);
+            this.UsuarioBox1.TabIndex = 2;
+            this.UsuarioBox1.TextChanged += new System.EventHandler(this.UsuarioBox1_TextChanged);
+            // 
+            // ContraseñaBox2
+            // 
+            this.ContraseñaBox2.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ContraseñaBox2.Location = new System.Drawing.Point(404, 371);
+            this.ContraseñaBox2.Name = "ContraseñaBox2";
+            this.ContraseñaBox2.PasswordChar = '*';
+            this.ContraseñaBox2.Size = new System.Drawing.Size(322, 31);
+            this.ContraseñaBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(404, 476);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(309, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "¿Olvidaste tu usuario y/o contraseña?";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(589, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 25);
+            this.label4.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(726, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 25);
+            this.label5.TabIndex = 6;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(404, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(322, 34);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Ingresar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Bright", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(12, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(862, 50);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "BIENVENIDOS A HOTEL CASA LOMAS";
+            this.label6.Click += new System.EventHandler(this.label6_Click_1);
+            // 
+            // FORMULARIO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(910, 617);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ContraseñaBox2);
+            this.Controls.Add(this.UsuarioBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "FORMULARIO";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox UsuarioBox1;
+        private TextBox ContraseñaBox2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button1;
+        private Label label6;
+    }
+}
---
+++ ProyectoHCL/Form1.cs
@@ -0,0 +1,45 @@
+namespace ProyectoHCL
+{
+    public partial class FORMULARIO : Form
+    {
+        public FORMULARIO()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UsuarioBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
