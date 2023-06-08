a7c4660fb2e9d902de74eaf6b15f18e67433bb97
OlmanDomínguez <gersondominguez.21@gmail.com>
2023-06-08 00:24:18 -0500

    Creacion de carpetas.

---
+++ ProyectoHCL/Datos usuario/BaseDatosHCL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ProyectoHCL.clases
+{
+    public class BaseDatosHCL
+    {
+
+        public static MySqlConnection ObtenerConexion()
+        {
+
+            MySqlConnection conectar = new MySqlConnection("server=containers-us-west-29.railway.app;port=6844; database = railway; Uid = root; pwd = LpxjPRi2Ckkz7FiKNUHn;");
+
+            conectar.Open();
+
+            return conectar;
+        }
+    }
+}
+
--- ProyectoHCL/Formularios/Form1.cs
+++ ProyectoHCL/Formularios/Form1.cs
@@ -1,7 +1,9 @@
 
 using System.Text;
 using System;
+using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using ProyectoHCL.clases;
 
 namespace ProyectoHCL
 {
@@ -43,35 +45,35 @@ namespace ProyectoHCL
 
         private void button1_Click(object sender, EventArgs e)
         {
-           // BaseDatosHCL.ObtenerConexion();
-            //MessageBox.Show("Conectado");
-            if (UsuarioBox1.Text == String.Empty)
-            {
-                errorProvider1.SetError(UsuarioBox1, "Ingrese un Usuario");
-                UsuarioBox1.Focus();
-                return;
-            }
-            errorProvider1.Clear();
-            if (string.IsNullOrEmpty(ContraseñaBox2.Text))
-            {
-                errorProvider1.SetError(ContraseñaBox2, "Ingrese una clave");
-                ContraseñaBox2.Focus();
-                return;
-            }
-            errorProvider1.Clear();
-            UsuarioDatos usuarioDatos = new UsuarioDatos();
-            bool usuarioValido = await usuarioDatos.ValidarUsuarioAsync(UsuarioBox1.Text, ContraseñaBox2.Text);
+            BaseDatosHCL.ObtenerConexion();
+            MessageBox.Show("Conectado");
+            /*if (UsuarioBox1.Text == String.Empty)
+             {
+                 errorProvider1.SetError(UsuarioBox1, "Ingrese un Usuario");
+                 UsuarioBox1.Focus();
+                 return;
+             }
+             errorProvider1.Clear();
+             if (string.IsNullOrEmpty(ContraseñaBox2.Text))
+             {
+                 errorProvider1.SetError(ContraseñaBox2, "Ingrese una clave");
+                 ContraseñaBox2.Focus();
+                 return;
+             }
+             errorProvider1.Clear();
+             UsuarioDatos usuarioDatos = new UsuarioDatos();
+             bool usuarioValido = await usuarioDatos.ValidarUsuarioAsync(UsuarioBox1.Text, ContraseñaBox2.Text);
 
-            if (usuarioValido == true)
-            {
-                PrincipalForm principalForm = new PrincipalForm();
-                this.Hide();
-                principalForm.Show();
-            }
-            else
-            {
-                MessageBox.Show("Datos de usuario incorrectos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
+             if (usuarioValido == true)
+             {
+                 PrincipalForm principalForm = new PrincipalForm();
+                 this.Hide();
+                 principalForm.Show();
+             }
+             else
+             {
+                 MessageBox.Show("Datos de usuario incorrectos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+             }*/
         }
 
 
