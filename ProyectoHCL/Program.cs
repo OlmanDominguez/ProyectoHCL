743132c7407a47e24bf8de087456f45d9d0c184d
OlmanDomínguez <gersondominguez.21@gmail.com>
2023-06-22 00:06:18 -0500

    listo

--- ProyectoHCL/Program.cs
+++ ProyectoHCL/Program.cs
@@ -13,7 +13,7 @@ namespace ProyectoHCL
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-          // Application.Run(new PermisosRoLes());
+           Application.Run(new PermisosRoLes());
             // Application.Run(new FORMULARIO());
           //  Application.Run(new RegistrarUsuario());
             Application.Run(new Dashboard());
