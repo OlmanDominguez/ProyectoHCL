b1a1b9dfb8b623513a755181d05d8a55bbf9c283
Suazo-H <Suazo-H@LAPTOP-BVCJA076>
2023-07-28 22:49:31 -0600

    Permisos

---
+++ ProyectoHCL/RolesPermisos/PermisoRol.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoHCL.RolesPermisos
+{
+    public class PermisoRol
+    {
+        public int IdPermisoRol { get; set; }
+
+        public int IdRol { get; set; }
+
+        public int IdPermiso { get; set; }
+
+        public int IdObjeto { get; set; }
+
+        public bool Permitido { get; set; }
+    }
+}
